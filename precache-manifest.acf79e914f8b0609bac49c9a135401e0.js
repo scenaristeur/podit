74c2ce8df5222e1f018ba28c8a9e55adf20c331e
Scenaristeur <scenaristeur@gmail.com>
2021-12-03 18:34:29 +0100

    new folder & file

---
+++ precache-manifest.acf79e914f8b0609bac49c9a135401e0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e743d3ed39e10cc0c23457a3f1cc685f",
+    "url": "/podit/404.html"
+  },
+  {
+    "revision": "0dbeffcc5b78db291d18",
+    "url": "/podit/css/app.be899b47.css"
+  },
+  {
+    "revision": "3cd5315d9d58609099ed",
+    "url": "/podit/css/chunk-20b5feb7.05d97788.css"
+  },
+  {
+    "revision": "a7a04a0a3c51d1db84d3",
+    "url": "/podit/css/chunk-vendors.8bda0bd5.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/podit/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4fd78ccce4ae35d3241fec5d9c0786be",
+    "url": "/podit/index.html"
+  },
+  {
+    "revision": "6fb2893f557a1d3180b7",
+    "url": "/podit/js/about.708aab9d.js"
+  },
+  {
+    "revision": "0dbeffcc5b78db291d18",
+    "url": "/podit/js/app.9ed52370.js"
+  },
+  {
+    "revision": "3cd5315d9d58609099ed",
+    "url": "/podit/js/chunk-20b5feb7.6d904782.js"
+  },
+  {
+    "revision": "bd2998550bf343c05bcd",
+    "url": "/podit/js/chunk-2d0aa17c.6d6da930.js"
+  },
+  {
+    "revision": "5771212ff57b63896ca7",
+    "url": "/podit/js/chunk-2d0c13e4.460a5ab3.js"
+  },
+  {
+    "revision": "3fb76d1ba088996497f5",
+    "url": "/podit/js/chunk-2d0dd031.18ebeae8.js"
+  },
+  {
+    "revision": "9afec5b797ce663f3e8a",
+    "url": "/podit/js/chunk-2d0e5f33.4ab1dbb6.js"
+  },
+  {
+    "revision": "07218ec0e5ece3951a07",
+    "url": "/podit/js/chunk-2d0e96e4.20800161.js"
+  },
+  {
+    "revision": "ff6658f03497ee9d9a10",
+    "url": "/podit/js/chunk-2d970383.11c4364b.js"
+  },
+  {
+    "revision": "9ff1dc5a94bac5b00cfe",
+    "url": "/podit/js/chunk-69d49705.b54c9251.js"
+  },
+  {
+    "revision": "00736d6b8eb9a19d0e5a",
+    "url": "/podit/js/chunk-e592f350.4fb9254b.js"
+  },
+  {
+    "revision": "a7a04a0a3c51d1db84d3",
+    "url": "/podit/js/chunk-vendors.3027deff.js"
+  },
+  {
+    "revision": "c0da6009d38f8283831b4aea24b4ca34",
+    "url": "/podit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/podit/robots.txt"
+  }
+]);
