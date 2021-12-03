f903cf6c4a76e74586c6a1c2367ff138cb4db2af
Scenaristeur <scenaristeur@gmail.com>
2021-12-03 09:31:40 +0100

    Initial dist subtree commit

---
+++ dist/precache-manifest.4e126417615d4fb48e8e1f430fde4ec2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e743d3ed39e10cc0c23457a3f1cc685f",
+    "url": "/podit/404.html"
+  },
+  {
+    "revision": "75a7db9df089fc4bfe4c",
+    "url": "/podit/css/app.7ff7b8b2.css"
+  },
+  {
+    "revision": "783367a29c978bd287d1",
+    "url": "/podit/css/chunk-20b5feb7.05d97788.css"
+  },
+  {
+    "revision": "2a554c659588a6908134",
+    "url": "/podit/css/chunk-vendors.8bda0bd5.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/podit/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6063515f44656ab498c84a4215657b87",
+    "url": "/podit/index.html"
+  },
+  {
+    "revision": "a8956b9961af95e6be7a",
+    "url": "/podit/js/about.b8f7a8c1.js"
+  },
+  {
+    "revision": "75a7db9df089fc4bfe4c",
+    "url": "/podit/js/app.c967930e.js"
+  },
+  {
+    "revision": "783367a29c978bd287d1",
+    "url": "/podit/js/chunk-20b5feb7.352cbc92.js"
+  },
+  {
+    "revision": "00736d6b8eb9a19d0e5a",
+    "url": "/podit/js/chunk-e592f350.4fb9254b.js"
+  },
+  {
+    "revision": "2a554c659588a6908134",
+    "url": "/podit/js/chunk-vendors.a7a66dca.js"
+  },
+  {
+    "revision": "c0da6009d38f8283831b4aea24b4ca34",
+    "url": "/podit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/podit/robots.txt"
+  }
+]);
